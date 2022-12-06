cd4de0a1e55d4de7671c9c47789787f7ca88617b
Eric Groom <bcgroom@gmail.com>
2022-12-06 12:40:01 -0800

    use Stream.chunk_every

--- lib/days/day_6.ex
+++ lib/days/day_6.ex
@@ -12,17 +12,8 @@ defmodule Advent2022.Days.Day6 do
   defp index_of_first_marker(input, unique_count) do
     index =
       input
-      |> Enum.scan([], fn el, acc ->
-        count = Enum.count(acc)
-
-        if count == unique_count do
-          [el | Enum.drop(acc, -1)]
-        else
-          [el | acc]
-        end
-      end)
-      |> Enum.drop(unique_count - 1)
-      |> Enum.map(&MapSet.new/1)
+      |> Stream.chunk_every(unique_count, 1)
+      |> Stream.map(&MapSet.new/1)
       |> Enum.find_index(fn el -> Enum.count(el) == unique_count end)
 
     index + unique_count
