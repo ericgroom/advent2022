dfe218a926096d3616c22e99791e5c841e166fd4
Eric Groom <bcgroom@gmail.com>
2022-12-07 22:08:21 -0800

    cleaned up day 8

--- lib/days/day_8.ex
+++ lib/days/day_8.ex
@@ -5,27 +5,13 @@ defmodule Advent2022.Days.Day8 do
   def part_one(input) do
     Grid.coords(input)
     |> Enum.map(fn coord ->
-      value_at_coord = Grid.at(input, coord)
+      tree_at_coord = Grid.at(input, coord)
 
       Vec2D.nondiagonal_unit_vectors()
       |> Enum.map(fn direction ->
-        Stream.unfold(coord, fn coord ->
-          next = Vec2D.add(direction, coord)
-          {next, next}
-        end)
-        |> Enum.reduce_while(true, fn test_coord, _acc ->
-          case Grid.at(input, test_coord) do
-            :invalid_coord ->
-              {:halt, true}
-
-            test_value ->
-              if test_value < value_at_coord do
-                {:cont, true}
-              else
-                {:halt, false}
-              end
-          end
-        end)
+        neighbors = neighbors_in_direction(input, coord, direction)
+        obscured = Enum.any?(neighbors, &(&1 >= tree_at_coord))
+        Enum.empty?(neighbors) or !obscured
       end)
       |> Enum.any?()
     end)
@@ -36,31 +22,43 @@ defmodule Advent2022.Days.Day8 do
   def part_two(input) do
     Grid.coords(input)
     |> Enum.map(fn coord ->
-      value_at_coord = Grid.at(input, coord)
+      tree_at_coord = Grid.at(input, coord)
 
       Vec2D.nondiagonal_unit_vectors()
       |> Enum.map(fn direction ->
-        Stream.unfold(coord, fn coord ->
-          next = Vec2D.add(direction, coord)
-          {next, next}
-        end)
-        |> Enum.reduce_while(0, fn test_coord, acc ->
-          case Grid.at(input, test_coord) do
-            :invalid_coord ->
-              {:halt, acc}
-
-            test_value ->
-              if test_value < value_at_coord do
-                {:cont, acc + 1}
-              else
-                {:halt, acc + 1}
-              end
-          end
-        end)
+        neighbors_in_direction(input, coord, direction)
+        |> take_while_inclusive(&(&1 < tree_at_coord))
+        |> Enum.count()
       end)
     end)
     |> Enum.map(&Enum.product/1)
     |> Enum.max()
+  end
+
+  defp neighbors_in_direction(grid, from, direction) do
+    Stream.unfold(from, fn coord ->
+      next = Vec2D.add(coord, direction)
+      {next, next}
+    end)
+    |> Enum.reduce_while([], fn coord, acc ->
+      case Grid.at(grid, coord) do
+        :invalid_coord -> {:halt, acc}
+        value -> {:cont, [value | acc]}
+      end
+    end)
+    |> Enum.reverse()
+  end
+
+  defp take_while_inclusive(enumerable, f) do
+    enumerable
+    |> Enum.reduce_while([], fn el, acc ->
+      if f.(el) do
+        {:cont, [el | acc]}
+      else
+        {:halt, [el | acc]}
+      end
+    end)
+    |> Enum.reverse()
   end
 
   def parse(raw) do
